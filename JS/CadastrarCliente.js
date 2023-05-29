86bff5baffca3002f1cf07f178a114889a7939ce
Vinicius-Nano <vinidenanoholanda@gmail.com>
2023-05-29 17:20:16 -0400

    Atualização geral (29/05)

--- JS/CadastrarCliente.js
+++ JS/CadastrarCliente.js
@@ -10,12 +10,33 @@ inputs.forEach((elemento) =>{
     
 });
 
+inputs.forEach((elemento) =>{
+    elemento.addEventListener("blur",(evento)=>{
+        console.log(evento);
+        validaCampo(evento.target);
+    })
+    
+    
+});
+
+function validaCampo(campo){
+ const msnErro = campo.parentNode.querySelector("[data-erro]");
+ if(campo.name === "nome"){
+    if(campo.value.length<5){
+        msnErro.textContent = "Digite o nome completo";
+    }else{
+        msnErro.textContent = "";
+    }
+ }
+}
 
 
-/*function atualizaItem(acao, inputQtde){
+
+function atualizaItem(acao, inputQtde){
     const item = inputQtde.querySelector("[required]");
     var str = item.value;
-    if(str.includes('@') === false){
+    if(str===""){
         item.value = "texto invalido";
     }
-}*/
+}
+
